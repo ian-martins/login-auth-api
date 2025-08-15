26484fc1e8eaf7d505dad3db6af5ec3d3e6f9821
Ian <ian.martins1@outlook.com>
2025-08-15 12:48:27 -0300

    first commit

---
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,47 @@
+package com.example.login_auth_api.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.login_auth_api.domain.User;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("secret")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpiraionDate()).sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    public String vaidateToken(String token){
+        try{
+            
+        }catch(JWTVerificationException){
+            return null;
+        }
+    }
+
+    private Instant generateExpiraionDate() {
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-3hours"));
+    }
+}
